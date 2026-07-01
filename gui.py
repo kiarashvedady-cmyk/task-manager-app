d273acd675bd3b49921db6efdbf73a39b2429145
kiarash <kiarashvedady@gmail.com>
2026-07-01 07:26:47 +0300

    handling the errors of complete and edit plus adding a clock

--- gui.py
+++ gui.py
@@ -1,27 +1,31 @@
-from os import remove
+from turtledemo import clock
 
-import functions
 import FreeSimpleGUI as Sg
+import functions
+import time
 
-
+Sg.theme("DarkGrey2")
 label = Sg.Text("type in a todo")
 input_box = Sg.InputText(tooltip="enter a todo :" , key="todo")
 add_button = Sg.Button("Add")
 list_box = Sg.Listbox(key="todos" , size = (45, 10) , values=functions.get_todos() , enable_events=True )
 edit_button = Sg.Button("Edit")
 exit_button = Sg.Button("Exit")
-remove_button = Sg.Button("Remove")
+complete_button = Sg.Button("Complete")
+clock1 = Sg.Text(" " , key="clock")
 
 
 my_window = Sg.Window("my to do" ,
-                      layout = [[label],[input_box , add_button] , [list_box , edit_button , remove_button] , [exit_button]] ,
+                      layout = [[clock1],
+                                [label],
+                                [input_box , add_button] ,
+                                [list_box , edit_button , complete_button] ,
+                                [exit_button]] ,
                       font=("Arial", 20),)
 
 while True:
-    event, values = my_window.read()
-    print("event",event)
-    print("values",values)
-    print("values listbox",values["todos"])
+    event, values = my_window.read(timeout=200)
+    my_window["clock"].update(value=time.strftime("%H:%M:%S"))
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -31,13 +35,17 @@ while True:
             my_window["todos"].update(values= todos)
 
         case "Edit":
-            todo_to_edit = values["todos"][0]
-            new_todo = values["todo"] + "\n"
-            todos = functions.get_todos()
-            index =todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            my_window["todos"].update(values=todos)
+            try:
+                todo_to_edit = values["todos"][0]
+                new_todo = values["todo"] + "\n"
+                todos = functions.get_todos()
+                index =todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_todos(todos)
+                my_window["todos"].update(values=todos)
+                my_window["todo"].update(value="")
+            except IndexError:
+                Sg.popup("select an item first" , font=("Arial", 20))
 
         case "todos" :
             my_window["todo"].update(value=values["todos"][0])
@@ -45,13 +53,16 @@ while True:
         case "Exit" :
             break
 
-        case "Remove" :
-            todos = functions.get_todos()
-            item_to_remove  = values["todos"][0]
-            todos.remove(item_to_remove)
-            functions.write_todos(todos)
-            my_window["todos"].update(values=todos)
-
+        case "Complete" :
+            try:
+                todos = functions.get_todos()
+                item_to_remove  = values["todos"][0]
+                todos.remove(item_to_remove)
+                functions.write_todos(todos)
+                my_window["todos"].update(values=todos)
+                my_window["todo"].update(value="")
+            except IndexError:
+                Sg.popup("select an item first" , font=("Arial", 20))
         case Sg.WINDOW_CLOSED:
             break
 my_window.close()
