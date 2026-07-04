5e17cec54445233ca6b716e24d9adf84dee6a3c5
kiarash <kiarashvedady@gmail.com>
2026-07-04 08:12:33 +0300

    final touches

--- gui.py
+++ gui.py
@@ -3,15 +3,20 @@ from turtledemo import clock
 import FreeSimpleGUI as Sg
 import functions
 import time
+import os
+
+if not os.path.exists('todos.txt'):
+    with open('todos.txt', 'w') as file:
+        pass
 
 Sg.theme("DarkGrey2")
 label = Sg.Text("type in a todo")
-input_box = Sg.InputText(tooltip="enter a todo :" , key="todo")
-add_button = Sg.Button("Add")
-list_box = Sg.Listbox(key="todos" , size = (45, 10) , values=functions.get_todos() , enable_events=True )
-edit_button = Sg.Button("Edit")
-exit_button = Sg.Button("Exit")
-complete_button = Sg.Button("Complete")
+input_box = Sg.InputText(tooltip="enter a todo :" , key="todo" , expand_x=True, expand_y=True)
+add_button = Sg.Button("Add" , expand_x=True, expand_y=True)
+list_box = Sg.Listbox(key="todos" , size = (45, 10) , values=functions.get_todos() , enable_events=True , expand_x=True, expand_y=True)
+edit_button = Sg.Button("Edit" , expand_x=True, expand_y=True)
+exit_button = Sg.Button("Exit" , expand_x=True, expand_y=True)
+complete_button = Sg.Button("Complete" , expand_x=True, expand_y=True)
 clock1 = Sg.Text(" " , key="clock")
 
 
@@ -21,7 +26,9 @@ my_window = Sg.Window("my to do" ,
                                 [input_box , add_button] ,
                                 [list_box , edit_button , complete_button] ,
                                 [exit_button]] ,
-                      font=("Arial", 20),)
+                      font=("Arial", 20),
+                      resizable=True,
+                      finalize=True)
 
 while True:
     event, values = my_window.read(timeout=200)
@@ -33,6 +40,7 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
             my_window["todos"].update(values= todos)
+            my_window["todo"].update(value="")
 
         case "Edit":
             try:
@@ -65,4 +73,5 @@ while True:
                 Sg.popup("select an item first" , font=("Arial", 20))
         case Sg.WINDOW_CLOSED:
             break
+
 my_window.close()
